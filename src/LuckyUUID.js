1eb91d4c0e72b59697bef9d2de8f4f1dd1b3a720
1s22s1 <1s22s1@users.noreply.github.com>
2025-07-11 11:45:03 +0900

    :art: Refactor

--- src/LuckyUUID.js
+++ src/LuckyUUID.js
@@ -1,16 +1,20 @@
+import { useLocation } from 'react-router-dom';
 import { TwitterShareButton, TwitterIcon } from 'react-share';
 import { v4 as uuidv4 } from 'uuid';
 
 const LuckyUUID = () => {
-    const text = `今日のラッキーUUIDは、${uuidv4()}です。`
+    const title = `今日のラッキーUUIDは、${uuidv4()}です。`
+
+    const location = useLocation();
+    const url = window.location.origin + location.pathname + location.search + location.hash;
 
     return (
         <>
-            <p>{text}</p>
+            <p>{title}</p>
 
             <hr />
 
-            <TwitterShareButton title={text}>
+            <TwitterShareButton title={title} url={url}>
                 <TwitterIcon size={32} round={true} />
             </TwitterShareButton>
         </>
